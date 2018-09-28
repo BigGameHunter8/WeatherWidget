5aa647873e3e1c19c12104a89d2f49a064b70311
Dmitri Zabiaco <Dmitri.Zabiaco@endava.com>
2018-09-28 15:41:52 +0300

    Correct temperature

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Forecast.scss';
 
+let celc = (far)=>{
+    return Math.floor((far-32)*5/9);
+}
+
 const Forecast = (props) => {
   return (
       <div className={ styles.atmoDiv }>
           <h1>{props.date}</h1>
-          <span>Minimal temperature is {props.low}℃ and maximum is {props.high}℃</span>
+          <span>Minimal temperature is {celc(props.low)}℃ and maximum is {celc(props.high)}℃</span>
           <div className={ styles.atmoDiv.bor }>Today will be {props.text}</div>
       </div>
   );
